8419332442b9ba70727c8cedad45e44cd345e5e0
Virus 24 <kazouya44@gmail.com>
2023-12-18 23:38:43 +0200

    update Ubuntu version add setup nodejs installer

--- setup-subsystem-linux.bat
+++ setup-subsystem-linux.bat
@@ -4,13 +4,13 @@ powershell -Command "Enable-WindowsOptionalFeature -Online -FeatureName Microsof
 :: Wait for a moment to allow the installation to complete
 timeout /t 10 /nobreak
 
-:: Install Ubuntu 18.04 (or your preferred version)
-wsl --list --quiet | findstr /i "Ubuntu-18.04"
+:: Install Ubuntu 22.04 (or your preferred version)
+wsl --list --quiet | findstr /i "Ubuntu-22.04"
 if %errorlevel% neq 0 (
-    echo Installing Ubuntu 18.04
-    wsl --install -d Ubuntu-18.04
+    echo Installing Ubuntu 22.04
+    wsl --install -d Ubuntu-22.04
 ) else (
-    echo Ubuntu 18.04 is already installed
+    echo Ubuntu 22.04 is already installed
 )
 
 wsl
