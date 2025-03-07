3e699127d95a9bfa1fde460e6d63118fe3b171b6
Sabry Dawood <kazouya25@gmail.com>
2025-03-07 02:01:54 +0200

    Refactor change user password script for WSL to simplify user input and remove unnecessary steps

--- Windows/wsl/change user pass.bat
+++ Windows/wsl/change user pass.bat
@@ -1,37 +1,8 @@
 @echo off
-setlocal enabledelayedexpansion
+wsl -l -v
 
-:: Get WSL distributions and store in a temporary file
-wsl -l -v > wsl_list.txt
+set /p WSL_NAME=Enter the WSL distribution Name: 
 
-:: Display options to the user
-set /a count=0
-for /f "skip=1 tokens=1,2,3" %%A in (wsl_list.txt) do (
-    set /a count+=1
-    echo %%A %%B %%C ^=> !count!
-    set "wsl[!count!]=%%A"
-)
+wsl -d %WSL_NAME% -u root -e bash -c "read -p 'Enter the username to change password: ' username; passwd $username"
 
-del wsl_list.txt
-
-echo.
-set /p choice=Enter the number of the WSL distribution: 
-
-:: Validate choice
-if not defined wsl[%choice%] (
-    echo Invalid choice. Exiting.
-    exit /b
-)
-
-set "wsl_name=!wsl[%choice%]!"
-
-echo Selected WSL: %wsl_name%
-
-:: Ask for username
-set /p username=Enter the username to change password: 
-
-:: Change password
-wsl -d %wsl_name% -u root -- passwd %username%
-
-echo Password changed successfully!
 pause
